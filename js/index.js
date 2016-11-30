e07a3499ae6bf5100d598104e4e872a67943dc56
夜舞暗澜 <18501292360@163.com>
2016-11-30 09:59:38 +0800

    v2.0
    整体重写的版本~

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$("a").attr("target","_blank");
+// var itemList=[
+//   {"name":"", "src":"", "describe":"", "url":""},
+//   {"name":"", "src":"", "describe":"", "url":""},
+// ];
+// for (i in itemList){
+//   var newItem='<div class="item"><img class="pic" src=""><div class="detail">123</div></div>';
+//   $('#pList').append();
+// }
