01e9a827a67e23ba6d82ae30721fed9001cea16e
YuanYayi <callmecrystal@163.com>
2018-03-31 14:29:39 +0800

    delete

--- js/index.js
+++
@@ -1,9 +0,0 @@
-$("a").attr("target","_blank");
-// var itemList=[
-//   {"name":"", "src":"", "describe":"", "url":""},
-//   {"name":"", "src":"", "describe":"", "url":""},
-// ];
-// for (i in itemList){
-//   var newItem='<div class="item"><img class="pic" src=""><div class="detail">123</div></div>';
-//   $('#pList').append();
-// }
